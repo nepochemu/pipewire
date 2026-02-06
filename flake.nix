399d2bb1eb48f68b1cdf641da9e92f566518d01f
airflower <nebosra@proton.me>
2026-02-06 20:47:14 +0100

    Add latency-ms option for tunnel sink

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         in {
           pwlink = pkgs.stdenvNoCC.mkDerivation {
             pname = "pwlink";
-            version = "0.1.3";
+            version = "0.1.4";
             src = self;
             nativeBuildInputs = [ pkgs.makeWrapper ];
             dontBuild = true;
